55a73af13250cdf89135a40d1f8f58618aeb38ed
startwords <marc.davernas@gmail.com>
2018-06-13 08:38:35 -0400

    altered tumblebooks.py checks to match current website.  Also, specified HTML parser

--- tumblebooks.py
+++ tumblebooks.py
@@ -6,13 +6,13 @@ for url in urls:
 
 	r = requests.get(url[1])
 
-	soup = BeautifulSoup(r.text)
+	soup = BeautifulSoup(r.text, "html.parser")
 
-	if soup.find_all("div", class_="col span_logo1"): 
+	if soup.find_all("img", alt="Read Online"): 
  		print str(count) +  " of " + str(num_lines) + " | " + "Right On!" 
  		outurls.writerow([url[0], url[1], "Right On!"])
  		count += 1
-	elif soup.find_all("div", class_="content-container"): 
+	elif soup.find_all("span", class_="lessonTitle"): 
 		print str(count) +  " of " + str(num_lines) + " | " + "Nope!" + " | " + url[1] + " | " + url[0]
 		outurls.writerow([url[0], url[1], "Nope!"])  
 		count += 1

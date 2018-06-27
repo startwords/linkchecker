59f16de7a73068b4c1fbb452ea5717f3d62258f4
startwords <marc.davernas@gmail.com>
2018-06-27 13:09:34 -0400

    added html parser and adjusting "right on!" to catch l360 interactives

--- l360.py
+++ l360.py
@@ -6,13 +6,14 @@ for url in urls:
 
 	r = requests.get(url[1])
 
-	soup = BeautifulSoup(r.text)
+	soup = BeautifulSoup(r.text, "html.parser")
 
-	if soup.find_all("div", class_="views"): 
+
+	if soup.find_all("div", id="ctl00_MainContent_titleDiv"): 
  		print str(count) +  " of " + str(num_lines) + " | " + "Right On!" 
  		outurls.writerow([url[0], url[1], "Right On!"])
  		count += 1
-	elif soup.find_all("div", class_="search-header"): 
+	elif soup.find_all("div", id="login"): 
 		print str(count) +  " of " + str(num_lines) + " | " + "Nope!" + " | " + url[1] + " | " + url[0]
 		outurls.writerow([url[0], url[1], "Nope!"])  
 		count += 1
